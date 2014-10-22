909cf7ed17c5b3c8b69cb4c0b4934e7853d85c29
Maheer <mi236@cornell.edu>
2014-10-22 16:04:52 -0400

    Instantiated arm only once for smoother movement and removed buffering

--- pose_control.cpp
+++ pose_control.cpp
@@ -7,7 +7,6 @@
 #include <tf/transform_listener.h>
 
 #define TGT_FRAME "/l_gripper_tool_frame"
-#define BUFFER_SIZE 5
 #define TRAJ_RATE 0.05 
 
 using namespace KDL; 
@@ -15,13 +14,7 @@ using namespace KDL;
 /**
 * Receive pose and orientation information from ROS topics and send it on to ee_cart for
 * simulator control.
-*/
-
-/* Contains the poses received so far */ 
-geometry_msgs::Pose poseBuffer[BUFFER_SIZE]; 
-
-/* Pointer into the pose buffer to see if it is full */
-int pointer; 
+*/ 
 
 /* KDL rotation from haptics frame to robot frame (column-major) */
 KDL::Rotation rotation = Rotation(0, 0, -1, -1, 0, 0, 0, 1, 0);  
@@ -33,15 +26,11 @@ tf::StampedTransform transform;
 geometry_msgs::Point synced_point; 
 
 /* Flag to check if this is the first message to allow syncing */
-bool first = true; 
+bool first = true;  
 
-void poseCallback(const geometry_msgs::PoseStamped::ConstPtr& stamped) {
-  poseBuffer[pointer] = stamped->pose;    
+void poseCallback(EECartImpedArm &arm, const geometry_msgs::PoseStamped::ConstPtr& stamped) {
 
   if (first) {
-    /*std::cout << "fin_x " << (finVector.x() + synced_point.x) << std::endl;
-    std::cout << "fin_y " << (finVector.y() + synced_point.y) << std::endl;
-    std::cout << "fin_z " << (finVector.z() + synced_point.z) << std::endl;*/
     geometry_msgs::Pose p = stamped->pose;     
     KDL::Vector hapVector = Vector(p.position.x, p.position.y, p.position.z);
     // Convert to robot coordinates   
@@ -52,57 +41,39 @@ void poseCallback(const geometry_msgs::PoseStamped::ConstPtr& stamped) {
     first = false; 
     }
 
-  if (pointer == (BUFFER_SIZE - 1)) {
-    EECartImpedArm arm("l_arm_cart_imped_controller");
-    ee_cart_imped_msgs::EECartImpedGoal traj;
-    ros::Rate rate(100.0);  
-    for (int i = 0; i < BUFFER_SIZE; i++) {
+  ee_cart_imped_msgs::EECartImpedGoal traj;  
 
-      geometry_msgs::Pose currPose = poseBuffer[i];
-      geometry_msgs::Quaternion qtn = currPose.orientation;
-      geometry_msgs::Point pnt = currPose.position; 
+  geometry_msgs::Pose currPose = stamped->pose;
+  geometry_msgs::Quaternion qtn = currPose.orientation;
+  geometry_msgs::Point pnt = currPose.position; 
 
-      /* Rotation given by the haptic quaternion */
-      KDL::Rotation qRotation = KDL::Rotation::Quaternion(qtn.x, qtn.y, qtn.z, qtn.w);
+  /* Rotation given by the haptic quaternion */
+  KDL::Rotation qRotation = KDL::Rotation::Quaternion(qtn.x, qtn.y, qtn.z, qtn.w);
 
-      /* Transforming from haptic frame to robot frame */
-      KDL::Rotation finRotation = rotation * qRotation; 
+  /* Transforming from haptic frame to robot frame */
+  KDL::Rotation finRotation = rotation * qRotation; 
 
-      /* Vector from the position of the haptic point */
-      KDL::Vector pVector = Vector(pnt.x, pnt.y, pnt.z);  
+  /* Vector from the position of the haptic point */
+  KDL::Vector pVector = Vector(pnt.x, pnt.y, pnt.z);  
 
-      /* Transforming position from haptic frame to robot frame */
-      KDL::Vector finVector = rotation * pVector; 
+  /* Transforming position from haptic frame to robot frame */
+  KDL::Vector finVector = rotation * pVector; 
 
-      /* Coordinates for the current robot Quaternion transform */
-      double ox;
-      double oy;
-      double oz;
-      double ow;
+  /* Coordinates for the current robot Quaternion transform */
+  double ox;
+  double oy;
+  double oz;
+  double ow;
 
-      /* Store the current quaternion of the rotation */
-      finRotation.GetQuaternion (ox, oy, oz, ow);  
+  /* Store the current quaternion of the rotation */
+  finRotation.GetQuaternion (ox, oy, oz, ow);  
 
-      /*std::cout << transform.getOrigin().x() << " " << synced_point.x << " " << finVector.y() << " " << i << std::endl;
-	std::cout << transform.getOrigin().y() << " " << synced_point.y << " " << finVector.y() << std::endl;
-	std::cout << transform.getOrigin().z() << " " << synced_point.z << " " << finVector.z() << std::endl;*/
-
-	//transform.getOrigin().x(), transform.getOrigin().y(), transform.getOrigin().z(),
-
-      EECartImpedArm::addTrajectoryPoint(traj, finVector.x() + synced_point.x, finVector.y() + synced_point.y, finVector.z() + synced_point.z, 
-				     0, 0, 0, 1,
-                                     1000, 1000, 1000, 30, 30, 30,
+  EECartImpedArm::addTrajectoryPoint(traj, finVector.x() + synced_point.x, finVector.y() + synced_point.y, finVector.z() + synced_point.z, 
+				     ox, oy, oz, ow,
+                                     2500, 2500, 2500, 100, 100, 100,
                                      false, false, false, false, false,
-					 false, TRAJ_RATE * (i + 1), "/torso_lift_link"); 
-      std::cout << "Time: " << ros::Time::now().toSec() - stamped->header.stamp.toSec() << std::endl; 
-      arm.startTrajectory(traj, false);
-      //rate.sleep(); 
-    }
-    pointer = 0; 
-  } // end of if statement 
-  else {
-    pointer++;
-  }
+					 false, TRAJ_RATE, "/torso_lift_link");  
+  arm.startTrajectory(traj, false);
 }
 
 int main(int argc, char **argv)
@@ -112,7 +83,10 @@ int main(int argc, char **argv)
 
   ros::NodeHandle n;
 
-  ros::Subscriber sub = n.subscribe("hapticsPose", 1000, poseCallback);
+  /* The arm we will be moving using the haptics information */
+  EECartImpedArm arm("l_arm_cart_imped_controller");
+
+  ros::Subscriber sub = n.subscribe<geometry_msgs::PoseStamped>("hapticsPose", 0, boost::bind(&poseCallback,boost::ref(arm),_1));
 
   //Store transform information for later use
 
